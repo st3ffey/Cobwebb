3ff56e43299d15be07b729127ee687f33dadd98f
Maxwell Steffey <119967345+st3ffey@users.noreply.github.com>
2024-04-25 14:07:23 -0400

    Update pipeline.py

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -18,7 +18,7 @@ def get_docs(query: str, top_k: int):
         chunk_text = x.metadata["chunk_test"]
         metadata = {k: v for k, v in x.metadata.items() if k != "chunk_test"}
         docs.append({"chunk_text": chunk_text, "metadata": metadata})
-    
+
     return docs
 
 def generate_response(query, docs, metadata, citations, conversation_history):
@@ -29,9 +29,10 @@ def generate_response(query, docs, metadata, citations, conversation_history):
         model='gpt-4-1106-preview',
         temperature=0.05,
         messages=[
-            {"role": "system", "content": "You are a helpful AI assistant, meant to provide an answer to the user \
-            question. If the user asks for company names and they aren't available in the text, provide the ticker."},
-            {"role": "user", "content": f"Conversation history:\n{history_string}\n\nQuery: {query}\nDocs, in a list: {docs}, and metadata: {metadata}"}
+            {"role": "system", "content": "You are Cobwebb, an AI assistant with expertise in executive compensation, \
+            meant to provide an answer to the user question. If the user asks for company names and they aren't available \
+            in the text, provide the ticker. You don't have to use all retrieved documents in your response, if not all of them are relevant to the question."},
+            {"role": "user", "content": f"Conversation history so far:\n{history_string}\n\nQuery: {query}\nDocs, in a list: {docs}, and metadata: {metadata}"}
         ]
     )
     return response.choices[0].message.content
@@ -44,7 +45,7 @@ def generate_alternative_question(query):
         model='gpt-3.5-turbo-0125',
         temperature=0.05,
         messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": "You are a helpful assistant meant to generate similar queries."},
         {"role": "user", "content": prompt}
     ]
     )
@@ -52,46 +53,46 @@ def generate_alternative_question(query):
     alternative_question = response.choices[0].message.content
     return alternative_question
 
-def get_chat_response(query, top_k=25, top_n=8, clear_history=False):
+def get_chat_response(query, top_k=30, top_n=9, clear_history=False):
     global conversation_history
 
     if clear_history:
         conversation_history=[]
-        
+
     # Generate alternative question
     alternative_question = generate_alternative_question(query)
-    
+
     # Combine the original query and alternative question
     expanded_query = query + ". In other terms, " + alternative_question
     print(expanded_query)
-    
+
     # Perform document retrieval using the expanded query
     docs = get_docs(expanded_query, top_k=top_k)
-    
+
     # Extract chunk_text and metadata from the retrieved documents
     documents = [doc["chunk_text"] for doc in docs]
     metadata = [doc["metadata"] for doc in docs]
-    
+
     # Rerank the documents
     rerank_docs = co.rerank(
         query=query,
         documents=documents,
         top_n=top_n,
-        model="rerank-english-v2.0"
+        model="rerank-english-v3.0"
     )
     reranked_candidates = rerank_docs.results
-    
+
     # Get the reranked document content and metadata
     doc_content = [documents[candidate.index] for candidate in reranked_candidates]
     doc_metadata = [metadata[candidate.index] for candidate in reranked_candidates]
-    
+
     # Generate citations for the reranked documents
     citations = "\n".join([f"{i+1}. {doc}\nMetadata: {doc_metadata[i]}" for i, doc in enumerate(doc_content)])
-    
+
     # Generate a response based on the reranked documents and conversation history
     response = generate_response(query, doc_content, doc_metadata, citations, conversation_history)
-    
+
     # Append the user's query and the model's response to the conversation history
     conversation_history.append((query, response))
-    
+
     return response, citations
